43d52d67137b4168b9838389137bbd63d0ddf284
antonippp <70588480+antonippp@users.noreply.github.com>
2020-09-01 16:27:48 +0200

    lol
    lol

---
+++ questions.js
@@ -0,0 +1,323 @@
+questions = [
+    {
+        "question": "Czy twój styl pisania jest charakterystyczny pod wzglądem specjalnie popełnianych błędów?",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Znak wodny innej strony na memie jest wystarczającym powodem do dania strzałki w dół.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Admin powinien banować za mały spam.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },    {
+        "question": "Jeja kiedyś było lepsze.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },    {
+        "question": "Czy ludzię często nazywają cię ''panem marudą''.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Jeja ma toksyczną społeczność.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Za treśći pornograficzne administarcja powinna dawać permanentnego bana.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },    {
+        "question": "Jeja się ''zweebiało''.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": -5
+        }
+    },    {
+        "question": "Czy podczas pisania komentarzy używasz polskich znaków?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Autorskie memy na jeja zazwyczaj są słabe.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Dodawanie tuneli powinno być dozwolone.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },    {
+        "question": "''Parówki'' są nieodłącznym elementem kultury jeja.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },    {
+        "question": "Czy ludzie kojarzą cię z niepoważnymi komentarzami?",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Memy na jeja nie są śmieszne.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Czy z chęcią wziąłbyś udział w jakiejś rewolucji na jeja.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -15,
+            "scty": 0
+        }
+    },    {
+        "question": "Z jeja ciągle odchodzą starzy użytkownicy i przechodzą nowi i jest to negatywem.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },    {
+        "question": "Czy twoje komentarze są często głupie i bezsensowne",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Czy jest powód dla którego lubisz/mógłbyś lubić jeja?",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Administracja powinna banować użytkowników dzielących się swoją kolekcją zdjęć z anime dziewcyznkami.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },    {
+        "question": "Możliwość dodawania własnych szablonów do generatora memów jest ogólnie korzystna.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },    {
+        "question": "Czy ludzie nazywają/nazywali cię kiedys trollem?",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Czcionka mema pochodząca z innej strony z memami powinna być powodem do zostawienia negatywnej oceny pod memem.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Czy boisz się dostać bana?",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },    {
+        "question": "Zmienianie loga jeja by mi nie przeszkadzało",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },    {
+        "question": "Czy często kłócisz się z ludźmi którzy cię ''baitują''?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Czy gdy widzisz mema wychwalającego jeja zostawiasz negatywną ocenę?",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Administracja jeja powinna mniej banować użytkowników.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },    {
+        "question": "Administracja powinna kontynuować żarty z okazji Primma Aprillis",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },    {
+        "question": "Czy często wyjaśniasz memy których inni nie rozumieją?",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Jeja ogólnie rzecz biorąc jest fajną stroną",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Zachowanie w którym: użytkownicy łamią regulamin, a następnie wściekają się na administrację za dostanie bana, jest głupie",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 10,
+            "scty": 0
+        }
+    },    {
+        "question": "Nocna zmiana jest czymś co integruje użytkowników",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },    {
+        "question": "Większość trolli jest nieśmieszna i głupia",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Większość stron internetowych z memami ma lepsze memy niż ma jeja.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Nawet mem będący częścią spamu może mieć możliwość przejścia na stronę główną.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },    {
+        "question": "Ocenianie za pomocą gwiazdek było gorsze niż ocenianie strzałkami",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },    {
+        "question": "Duża część trolli ma dobre poczucie humoru i jest zabawniejsza od normalnych użytkowników.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },    {
+        "question": "Przez administrację poczekalnia memów stała się ''syfem''",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": -10,
+            "scty": 0
+        }
+    },    {
+        "question": "Administracja wspiera ''konfidenctwo'' i jest to problemem.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },    {
+        "question": "Administracja powinna usuwać działy które nie przynoszą korzyści.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    }
+];
